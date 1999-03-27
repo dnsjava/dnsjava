59eb01095bd1eb9fd662050e0ff37b0c31fb0d56
bwelling <bwelling@c76caeb1-94fd-44dd-870f-0c9d92034fc1>
1999-03-27 05:17:08 +0000

    Added alg definitions
    git-svn-id: http://svn.code.sf.net/p/dnsjava/code/trunk@99 c76caeb1-94fd-44dd-870f-0c9d92034fc1

---
+++ org/xbill/DNS/DNSSEC.java
@@ -0,0 +1,12 @@
+// Copyright (c) 1999 Brian Wellington (bwelling@xbill.org)
+// Portions Copyright (c) 1999 Network Associates, Inc.
+
+package DNS;
+
+public class DNSSEC {
+
+public static final byte RSA = 1;
+public static final byte DH = 2;
+public static final byte DSA = 3;
+
+}
