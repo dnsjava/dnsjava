21e760a1db19e759dc280fb86b37e1a5ae2431c6
bwelling <bwelling@c76caeb1-94fd-44dd-870f-0c9d92034fc1>
1999-03-28 22:51:58 +0000

    Added DNSSEC stuff and toString
    git-svn-id: http://svn.code.sf.net/p/dnsjava/code/trunk@112 c76caeb1-94fd-44dd-870f-0c9d92034fc1

--- DNS/RRset.java
+++ DNS/RRset.java
@@ -9,11 +9,13 @@ import java.io.*;
 public class RRset {
 
 private Vector rrs;
+private Vector sigs;
 private Name name;
 private short type;
 
 public RRset(Name _name, short _type) {
 	rrs = new Vector();
+	sigs = new Vector();
 	name = _name;
 	type = _type;
 }
@@ -23,9 +25,42 @@ addRR(Record r) {
 	rrs.addElement(r);
 }
 
+public void
+addSIG(SIGRecord r) {
+	sigs.addElement(r);
+}
+
 public Enumeration
 rrs() {
 	return rrs.elements();
+}
+
+public Enumeration
+sigs() {
+	return sigs.elements();
+}
+
+public String
+toString() {
+	StringBuffer sb = new StringBuffer();
+	sb.append("{ [");
+	Enumeration e = rrs();
+	while (e.hasMoreElements()) {
+		Record rr = (Record) e.nextElement();
+		sb.append(rr);
+		if (e.hasMoreElements())
+			sb.append("<>");
+	}
+	sb.append("] [");
+	e = sigs();
+	while (e.hasMoreElements()) {
+		Record rr = (Record) e.nextElement();
+		sb.append(rr);
+		if (e.hasMoreElements())
+			sb.append("<>");
+	}
+	sb.append("] }");
+	return sb.toString();
 }
 
 }
