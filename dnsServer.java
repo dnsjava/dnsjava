fc55ac48a0544b19e1128d2798195cb718697d76
bwelling <bwelling@c76caeb1-94fd-44dd-870f-0c9d92034fc1>
1999-03-29 04:59:35 +0000

    Added authority/additional data, deals with truncation.
    git-svn-id: http://svn.code.sf.net/p/dnsjava/code/trunk@120 c76caeb1-94fd-44dd-870f-0c9d92034fc1

--- dnsServer.java
+++ dnsServer.java
@@ -1,3 +1,4 @@
+import java.lang.reflect.*;
 import java.io.*;
 import java.net.*;
 import java.util.*;
@@ -18,8 +19,9 @@ dnsServer() {
 
 public void
 addZone(String zonefile, int type) throws IOException {
-	Zone newzone = new Zone(zonefile, type);
+	Zone newzone = new Zone(zonefile, type, zones[0]);
 	znames.put(newzone.getOrigin(), newzone);
+/*System.out.println("Adding zone named <" + newzone.getOrigin() + ">");*/
 	zones[zcount++] = newzone;
 };
 
@@ -28,15 +30,88 @@ findBestZone(Name name) {
 	Zone foundzone = null;
 	Name tname = name;
 	do {
-		if (tname.equals(Name.root))
-			return null;
-		tname = new Name(tname, 1);
+/*System.out.println("Looking for zone named <" + tname + ">");*/
 		foundzone = (Zone) znames.get(tname);
-	} while (foundzone == null);
-	return foundzone;
+		if (foundzone != null)
+			return foundzone;
+		tname = new Name(tname, 1);
+	} while (!tname.equals(Name.root));
+	return null;
 }
 
-public Message
+public RRset
+findExactMatch(Name name, short type, short dclass) {
+	Zone zone = findBestZone(name);
+	Hashtable sets = zone.findName(name);
+	if (sets == null)
+		return null;
+	Short Type = new Short(type);
+	RRset rrset = (RRset) sets.get(Type);
+	return rrset;
+	
+}
+
+void
+addRRset(Message response, RRset rrset) {
+	Enumeration e = rrset.rrs();
+	while (e.hasMoreElements()) {
+		Record r = (Record) e.nextElement();
+		response.addRecord(Section.ANSWER, r);
+	}
+}
+
+
+void
+addAuthority(Message response, Zone zone) {
+	if (response.getHeader().getCount(Section.ANSWER) > 0) {
+		RRset nsRecords = (RRset) zone.getNS();
+		Enumeration e = nsRecords.rrs();
+		while (e.hasMoreElements()) {
+			Record r = (Record) e.nextElement();
+			if (response.findRecord(Section.ANSWER, r) == false)
+				response.addRecord(Section.AUTHORITY, r);
+		}
+	}
+	else {
+		SOARecord soa = (SOARecord) zone.getSOA();
+		response.addRecord(Section.AUTHORITY, soa);
+	}
+}
+
+private void
+addGlue(Message response, Name name) {
+	RRset a = findExactMatch(name, Type.A, DClass.IN);
+	Enumeration e = a.rrs();
+	while (e.hasMoreElements()) {
+			Record r = (Record) e.nextElement();
+			if (response.findRecord(r) == false)
+				response.addRecord(Section.ADDITIONAL, r);
+	}
+}
+
+private void
+addAdditional2(Message response, int section) {
+	Enumeration e = response.getSection(section);
+	while (e.hasMoreElements()) {
+		Record r = (Record) e.nextElement();
+		try {
+			Method m = r.getClass().getMethod("getTarget", null);
+			Name glueName = (Name) m.invoke(r, null);
+			addGlue(response, glueName);
+		}
+		catch (Exception ex) {
+		}
+	}
+	
+}
+
+void
+addAdditional(Message response) {
+	addAdditional2(response, Section.ANSWER);
+	addAdditional2(response, Section.AUTHORITY);
+}
+
+Message
 generateReply(Message query) {
 	Enumeration qds = query.getSection(Section.QUESTION);
 	Record queryRecord = (Record) qds.nextElement();
@@ -54,21 +129,70 @@ generateReply(Message query) {
 		response.getHeader().setRcode(Rcode.SERVFAIL);
 	}
 	else {
+/*System.out.println("Looking up name <" + name + "> in [" + zone.getOrigin() + "]");*/
 		Hashtable nameRecords = (Hashtable) zone.findName(name);
-		Short Type = new Short(type);
-		RRset responseRecords = (RRset) nameRecords.get(Type);
-		if (responseRecords == null) {
+		if (nameRecords == null) {
 			response.getHeader().setRcode(Rcode.NXDOMAIN);
 		}
 		else {
-			Enumeration e = responseRecords.rrs();
-			while (e.hasMoreElements()) {
-				Record r = (Record) e.nextElement();
-				response.addRecord(Section.ANSWER, r);
+			if (type == Type.ANY) {
+				Enumeration e = nameRecords.elements();
+				while (e.hasMoreElements()) {
+					RRset rrset = (RRset) e.nextElement();
+					addRRset(response, rrset);
+				}
+			}
+			else {
+				Short Type = new Short(type);
+				RRset rrset = (RRset) nameRecords.get(Type);
+				if (rrset != null)
+					addRRset(response, rrset);
 			}
 		}
+		addAuthority(response, zone);
+		addAdditional(response);
 	}
 	return response;
+}
+
+public int
+truncateSection(Message in, int maxLength, int length, int section) {
+	int removed = 0;
+	Record [] records = in.getSectionArray(section);
+	for (int i = records.length - 1; i >= 0; i--) {
+		Record r = records[i];
+		removed += r.getWireLength();
+		length -= r.getWireLength();
+		in.removeRecord(section, r);
+		if (length > maxLength)
+			continue;
+		else {
+			for (int j = i - 1; j >= 0; j--) {
+				Record r2 = records[j];
+				if (!r.getName().equals(r2.getName()) ||
+				    r.getType() != r2.getType() ||
+				    r.getDClass() != r2.getDClass())
+					break;
+				removed += r2.getWireLength();
+				length -= r2.getWireLength();
+				in.removeRecord(section, r2);
+			}
+			return removed;
+		}
+	}
+	return removed;
+}
+
+public void
+truncate(Message in, int length, int maxLength) {
+	length -= truncateSection(in, maxLength, length, Section.ADDITIONAL);
+	if (length < maxLength)
+		return;
+	in.getHeader().setFlag(Flags.TC);
+	length -= truncateSection(in, maxLength, length, Section.AUTHORITY);
+	if (length < maxLength)
+		return;
+	length -= truncateSection(in, maxLength, length, Section.ANSWER);
 }
 
 public void
@@ -125,6 +249,10 @@ serveUDP(short port) {
 			Message query = new Message(in);
 			Message response = generateReply(query);
 			byte [] out = response.toWire();
+			if (out.length > 512) {
+				truncate(response, out.length, 512);
+				out = response.toWire();
+			}
 			dp = new DatagramPacket(out, out.length,
 						dp.getAddress(), dp.getPort());
 			sock.send(dp);
