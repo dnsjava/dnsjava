6474732d36126946b6a645130bc4cfb17a5342c4
bwelling <bwelling@c76caeb1-94fd-44dd-870f-0c9d92034fc1>
1999-04-02 05:44:58 +0000

    Added CACHE
    git-svn-id: http://svn.code.sf.net/p/dnsjava/code/trunk@160 c76caeb1-94fd-44dd-870f-0c9d92034fc1

--- DNS/Credibility.java
+++ DNS/Credibility.java
@@ -5,16 +5,17 @@ package DNS;
 
 public final class Credibility {
 
-public static final byte NONAUTH_ADDITIONAL	= 0;
-public static final byte NONAUTH_AUTHORITY	= 0;
-public static final byte AUTH_ADDITIONAL	= 0;
-public static final byte NONAUTH_ANSWER		= 1;
-public static final byte AUTH_NONAUTH_ANSWER	= 1;
-public static final byte ZONE_TRANSFER_GLUE	= 2;
-public static final byte ZONE_GLUE		= 2;
-public static final byte AUTH_AUTHORITY		= 3;
-public static final byte AUTH_ANSWER		= 4;
-public static final byte ZONE_TRANSFER		= 5;
-public static final byte ZONE			= 6;
+public static final byte CACHE			= 0;
+public static final byte NONAUTH_ADDITIONAL	= 1;
+public static final byte NONAUTH_AUTHORITY	= 1;
+public static final byte AUTH_ADDITIONAL	= 1;
+public static final byte NONAUTH_ANSWER		= 2;
+public static final byte AUTH_NONAUTH_ANSWER	= 2;
+public static final byte ZONE_TRANSFER_GLUE	= 3;
+public static final byte ZONE_GLUE		= 3;
+public static final byte AUTH_AUTHORITY		= 4;
+public static final byte AUTH_ANSWER		= 5;
+public static final byte ZONE_TRANSFER		= 6;
+public static final byte ZONE			= 7;
 
 }
