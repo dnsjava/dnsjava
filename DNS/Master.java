34ccd4acb95158f032571b011356e923e2dc3dc7
bwelling <bwelling@c76caeb1-94fd-44dd-870f-0c9d92034fc1>
1999-04-02 23:08:36 +0000

    Master file parser
    git-svn-id: http://svn.code.sf.net/p/dnsjava/code/trunk@163 c76caeb1-94fd-44dd-870f-0c9d92034fc1

---
+++ DNS/Master.java
@@ -0,0 +1,95 @@
+// Copyright (c) 1999 Brian Wellington (bwelling@xbill.org)
+// Portions Copyright (c) 1999 Network Associates, Inc.
+
+package DNS;
+
+import java.util.*;
+import java.io.*;
+
+public class Master {
+
+private Name origin = null;
+private BufferedReader br;
+private Record last = null;
+
+public
+Master(String file) throws IOException {
+	FileInputStream fis;
+	try {
+		fis = new FileInputStream(file);
+	}
+	catch (FileNotFoundException e) {
+		throw new IOException(e.toString());
+	}
+	br = new BufferedReader(new InputStreamReader(fis));
+}
+
+public Record
+nextRecord() throws IOException {
+	String line;
+	MyStringTokenizer st;
+
+	while (true) {
+		line = IO.readExtendedLine(br);
+		if (line == null)
+			return null;
+		if (line.length() == 0 || line.startsWith(";"))
+			continue;
+
+		boolean space = line.startsWith(" ") || line.startsWith("\t");
+		st = new MyStringTokenizer(line);
+
+		String s = st.nextToken();
+		if (s.equals("$ORIGIN")) {
+			origin = parseOrigin(st);
+			continue;
+		}
+		st.putBackToken(s);
+		return (last = parseRR(st, space, last, origin));
+	}
+}
+
+Name
+parseOrigin(MyStringTokenizer st) throws IOException {
+	return new Name(st.nextToken());
+}
+
+Record
+parseRR(MyStringTokenizer st, boolean useLast, Record last, Name origin)
+throws IOException
+{
+	Name name;
+	int ttl;
+	short type, dclass;
+
+	if (!useLast)
+		name = new Name(st.nextToken(), origin);
+	else
+		name = last.name;
+
+	String s = st.nextToken();
+
+	try {
+		ttl = TTL.parseTTL(s);
+		s = st.nextToken();
+	}
+	catch (NumberFormatException e) {
+		if (!useLast || last == null)
+			ttl = 3600;
+		else
+			ttl = last.ttl;
+	}
+
+	if ((dclass = DClass.value(s)) > 0)
+		s = st.nextToken();
+	else
+		dclass = DClass.IN;
+		
+
+	if ((type = Type.value(s)) < 0)
+		throw new IOException("Parse error");
+
+	return Record.fromString(name, type, dclass, ttl, st, origin);
+}
+
+}
