93b6367e4d5d7266a0e190c468ee411d60984694
bwelling <bwelling@c76caeb1-94fd-44dd-870f-0c9d92034fc1>
1999-04-04 06:23:03 +0000

    CACHE should have been HINT
    git-svn-id: http://svn.code.sf.net/p/dnsjava/code/trunk@173 c76caeb1-94fd-44dd-870f-0c9d92034fc1

--- DNS/Credibility.java
+++ DNS/Credibility.java
@@ -5,7 +5,7 @@ package DNS;
 
 public final class Credibility {
 
-public static final byte CACHE			= 0;
+public static final byte HINT			= 0;
 public static final byte NONAUTH_ADDITIONAL	= 1;
 public static final byte NONAUTH_AUTHORITY	= 1;
 public static final byte AUTH_ADDITIONAL	= 1;
