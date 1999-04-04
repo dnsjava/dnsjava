06230bc00677ac17fa9015f2a3a5633b176d0565
bwelling <bwelling@c76caeb1-94fd-44dd-870f-0c9d92034fc1>
1999-04-04 06:23:25 +0000

    added negative caching, general cleanup
    git-svn-id: http://svn.code.sf.net/p/dnsjava/code/trunk@174 c76caeb1-94fd-44dd-870f-0c9d92034fc1

--- DNS/Cache.java
+++ DNS/Cache.java
@@ -15,21 +15,33 @@ private class CacheElement {
 	byte credibility;
 	long timeIn;
 	int ttl;
+	int srcid;
 
 	public
-	CacheElement(Record r, byte cred) {
+	CacheElement(int _ttl, byte cred, int src) {
+		rrset = null;
+		credibility = cred;
+		ttl = _ttl;
+		srcid = src;
+		timeIn = System.currentTimeMillis();
+	}
+	public
+	CacheElement(Record r, byte cred, int src) {
 		rrset = new RRset();
 		credibility = cred;
 		ttl = -1;
+		srcid = src;
 		update(r);
 	}
+
 	public
-	CacheElement(RRset r, byte cred) {
+	CacheElement(RRset r, byte cred, int src) {
 		rrset = r;
 		credibility = cred;
 		ttl = -1;
 		timeIn = System.currentTimeMillis();
 		ttl = r.getTTL();
+		srcid = src;
 	}
 
 	public void
@@ -65,32 +77,45 @@ Cache(String file) throws IOException {
 	Master m = new Master(file);
 	Record record;
 	while ((record = m.nextRecord()) != null) {
-		addRecord(record, Credibility.CACHE);
+		addRecord(record, Credibility.HINT, m);
 	}
 }
 
 public void
-addRecord(Record r, byte cred) {
+addRecord(Record r, byte cred, Object o) {
 	Name name = r.getName();
 	short type = r.getRRsetType();
+	int src = (o != null) ? o.hashCode() : 0;
 	if (r.getTTL() == 0)
 		return;
 	CacheElement element = (CacheElement) findSet(name, type);
 	if (element == null || cred > element.credibility)
-		addSet(name, type, element = new CacheElement(r, cred));
-	else if (cred == element.credibility)
+		addSet(name, type, element = new CacheElement(r, cred, src));
+	else if (cred == element.credibility) {
+		if (element.srcid != src)
+			element.rrset.clear();
 		element.update(r);
+	}
 }
 
 public void
-addRRset(RRset rrset, byte cred) {
+addRRset(RRset rrset, byte cred, Object o) {
 	Name name = rrset.getName();
 	short type = rrset.getType();
+	int src = (o != null) ? o.hashCode() : 0;
 	if (rrset.getTTL() == 0)
 		return;
 	CacheElement element = (CacheElement) findSet(name, type);
 	if (element == null || cred > element.credibility)
-		addSet(name, type, new CacheElement(rrset, cred));
+		addSet(name, type, new CacheElement(rrset, cred, src));
+}
+
+public void
+addNegative(Name name, short type, int ttl, byte cred, Object o) {
+	int src = (o != null) ? o.hashCode() : 0;
+	CacheElement element = (CacheElement) findSet(name, type);
+	if (element == null || cred > element.credibility)
+		addSet(name, type, new CacheElement(ttl, cred, src));
 }
 
 private RRset
@@ -104,6 +129,9 @@ findRecords(Name name, short type, byte minCred) {
 	}
 	if (element.credibility >= minCred) {
 		RRset rrset = element.rrset;
+		/* Negative cached response */
+		if (rrset == null)
+			return null;
 		if (type != Type.CNAME && rrset.getType() == Type.CNAME) {
 			CNAMERecord cname;
 			cname = (CNAMERecord) rrset.rrs().nextElement();
@@ -131,36 +159,55 @@ addMessage(Message in) {
 	Enumeration e;
 	boolean isAuth = in.getHeader().getFlag(Flags.AA);
 	Name queryName = in.getQuestion().getName();
+	short queryType = in.getQuestion().getType();
+	byte cred;
 
 	e = in.getSection(Section.ANSWER);
+	if (!e.hasMoreElements()) {
+		/* This is a negative response.  Try to cache it */
+		e = in.getSection(Section.AUTHORITY);
+		while (e.hasMoreElements()) {
+			Record r = (Record) e.nextElement();
+			if (r.getType() != Type.SOA)
+				continue;
+			if (isAuth)
+				cred = Credibility.AUTH_AUTHORITY;
+			else
+				cred = Credibility.NONAUTH_AUTHORITY;
+			SOARecord soa = (SOARecord) r;
+			int ttl = Math.min(soa.getTTL(), soa.getMinimum());
+			addNegative(queryName, queryType, ttl, cred, in);
+		}
+	}
 	while (e.hasMoreElements()) {
 		Record r = (Record) e.nextElement();
-		if (isAuth) {
-			if (r.getName().equals(queryName))
-				addRecord(r, Credibility.AUTH_ANSWER);
-			else
-				addRecord(r, Credibility.AUTH_NONAUTH_ANSWER);
-		}
+		if (isAuth && r.getName().equals(queryName))
+			cred = Credibility.AUTH_ANSWER;
+		else if (isAuth)
+			cred = Credibility.AUTH_NONAUTH_ANSWER;
 		else
-			addRecord(r, Credibility.NONAUTH_ANSWER);
+			cred = Credibility.NONAUTH_ANSWER;
+		addRecord(r, cred, in);
 	}
 
 	e = in.getSection(Section.AUTHORITY);
 	while (e.hasMoreElements()) {
 		Record r = (Record) e.nextElement();
 		if (isAuth)
-			addRecord(r, Credibility.AUTH_AUTHORITY);
+			cred = Credibility.AUTH_AUTHORITY;
 		else
-			addRecord(r, Credibility.NONAUTH_AUTHORITY);
+			cred = Credibility.NONAUTH_AUTHORITY;
+		addRecord(r, cred, in);
 	}
 
 	e = in.getSection(Section.ADDITIONAL);
 	while (e.hasMoreElements()) {
 		Record r = (Record) e.nextElement();
 		if (isAuth)
-			addRecord(r, Credibility.AUTH_ADDITIONAL);
+			cred = Credibility.AUTH_ADDITIONAL;
 		else
-			addRecord(r, Credibility.NONAUTH_ADDITIONAL);
+			cred = Credibility.NONAUTH_ADDITIONAL;
+		addRecord(r, cred, in);
 	}
 }
 
