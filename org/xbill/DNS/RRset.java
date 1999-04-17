f7615a32a98297426e9d05f3a432e20a23e31b85
bwelling <bwelling@c76caeb1-94fd-44dd-870f-0c9d92034fc1>
1999-04-17 05:28:16 +0000

    javadoc
    git-svn-id: http://svn.code.sf.net/p/dnsjava/code/trunk@270 c76caeb1-94fd-44dd-870f-0c9d92034fc1

--- org/xbill/DNS/RRset.java
+++ org/xbill/DNS/RRset.java
@@ -6,16 +6,26 @@ package DNS;
 import java.util.*;
 import java.io.*;
 
+/**
+ * A set of Records with the same name, type, and class.  Also included
+ * are all SIG records signing the data records.
+ * @see Record
+ * @see SIGRecord
+ */
+
 public class RRset {
 
 private Vector rrs;
 private Vector sigs;
 
-public RRset() {
+/** Creates an empty RRset */
+public
+RRset() {
 	rrs = new Vector();
 	sigs = new Vector();
 }
 
+/** Adds a Record to an RRset */
 public void
 addRR(Record r) {
 	if (r.getType() != Type.SIG) {
@@ -28,47 +38,72 @@ addRR(Record r) {
 	}
 }
 
+/** Deletes all Records from an RRset */
 public void
 clear() {
 	rrs.setSize(0);
 	sigs.setSize(0);
 }
 
+/** Returns an Enumeration listing all (data) records */
 public Enumeration
 rrs() {
 	return rrs.elements();
 }
 
+/** Returns an Enumeration listing all signature records */
 public Enumeration
 sigs() {
 	return sigs.elements();
 }
 
+/** Returns the number of (data) records */
 public int
 size() {
 	return rrs.size();
 }
 
+/**
+ * Returns the name of the records
+ * see @Name
+ */
 public Name
 getName() {
+	if (rrs.size() == 0)
+		return null;
 	Record r =  (Record) rrs.elementAt(0);
 	return r.getName();
 }
 
+/**
+ * Returns the type of the records
+ * see @Type
+ */
 public short
 getType() {
+	if (rrs.size() == 0)
+		return 0;
 	Record r =  (Record) rrs.elementAt(0);
 	return r.getType();
 }
 
+/**
+ * Returns the class of the records
+ * see @DClass
+ */
 public short
 getDClass() {
+	if (rrs.size() == 0)
+		return 0;
 	Record r =  (Record) rrs.elementAt(0);
 	return r.getDClass();
 }
 
+/** Returns the ttl of the records */
 public int
 getTTL() {
+	if (rrs.size() == 0)
+		return 0;
 	int ttl = Integer.MAX_VALUE;
 	Enumeration e = rrs();
 	while (e.hasMoreElements()) {
@@ -79,6 +114,7 @@ getTTL() {
 	return ttl;
 }
 
+/** Converts the RRset to a String */
 public String
 toString() {
 	StringBuffer sb = new StringBuffer();
