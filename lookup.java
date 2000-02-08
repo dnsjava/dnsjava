f0462424b7a3e9b2ea13f43c065d3ac9514ae2b8
bwelling <bwelling@c76caeb1-94fd-44dd-870f-0c9d92034fc1>
2000-02-08 19:21:43 +0000

    simple name lookup
    git-svn-id: http://svn.code.sf.net/p/dnsjava/code/trunk@499 c76caeb1-94fd-44dd-870f-0c9d92034fc1

---
+++ lookup.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import org.xbill.DNS.*;
+
+public class lookup {
+
+public static void
+printAnswer(String name, Record [] answer) {
+	System.out.println(name + ":");
+	if (answer == null)
+		System.out.println("null");
+	else
+		for (int i = 0; i < answer.length; i++)
+			System.out.println(answer[i]);
+}
+
+public static void main(String [] args) throws Exception {
+	for (int i = 0; i < args.length; i++)
+		printAnswer(args[i], dns.getRecords(args[i], Type.A));
+}
+
+}
