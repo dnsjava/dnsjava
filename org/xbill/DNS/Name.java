4ace13690f45cf8d79f1210b009b1f584490bfdb
bwelling <bwelling@c76caeb1-94fd-44dd-870f-0c9d92034fc1>
2002-10-22 05:00:28 +0000

    Make the toWire()/toWireCanonical() methods that take DataByteOutputStreams package private.
    git-svn-id: http://svn.code.sf.net/p/dnsjava/code/trunk@880 c76caeb1-94fd-44dd-870f-0c9d92034fc1

--- org/xbill/DNS/Name.java
+++ org/xbill/DNS/Name.java
@@ -604,7 +604,7 @@ getLabelString(int n) {
  * @param c The compression context, or null of no compression is desired.
  * @throws IllegalArgumentException The name is not absolute.
  */
-public void
+void
 toWire(DataByteOutputStream out, Compression c) {
 	if (!isAbsolute())
 		throw new IllegalArgumentException("toWire() called on " +
@@ -648,7 +648,7 @@ toWire() {
  * Convert Name to canonical DNS wire format (all lowercase)
  * @param out The output stream to which the message is written.
  */
-public void
+void
 toWireCanonical(DataByteOutputStream out) {
 	byte [] b = toWireCanonical();
 	out.writeArray(b);
