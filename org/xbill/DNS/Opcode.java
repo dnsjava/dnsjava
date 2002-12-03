d08388fe4de36a90f07ad628a2e7434cc5752fd3
bwelling <bwelling@c76caeb1-94fd-44dd-870f-0c9d92034fc1>
2002-12-03 06:24:18 +0000

    Add constants and strings for IQUERY and STATUS opcodes.
    git-svn-id: http://svn.code.sf.net/p/dnsjava/code/trunk@896 c76caeb1-94fd-44dd-870f-0c9d92034fc1

--- org/xbill/DNS/Opcode.java
+++ org/xbill/DNS/Opcode.java
@@ -15,8 +15,14 @@ public final class Opcode {
 
 private static StringValueTable opcodes = new StringValueTable();
 
-/** A query sent to a server */
+/** A standard query */
 public static final byte QUERY		= 0;
+
+/** An inverse query (deprecated) */
+public static final byte IQUERY		= 1;
+
+/** A server status request (not used) */
+public static final byte STATUS		= 2;
 
 /**
  * A message from a primary to a secondary server to initiate a zone transfer
@@ -28,6 +34,8 @@ public static final byte UPDATE		= 5;
 
 static {
 	opcodes.put2(QUERY, "QUERY");
+	opcodes.put2(QUERY, "IQUERY");
+	opcodes.put2(QUERY, "STATUS");
 	opcodes.put2(NOTIFY, "NOTIFY");
 	opcodes.put2(UPDATE, "UPDATE");
 }
