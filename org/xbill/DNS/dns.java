af0f22102efe4aaf7ff4c1ac92ec83b94b0fa746
bwelling <bwelling@c76caeb1-94fd-44dd-870f-0c9d92034fc1>
2002-12-10 05:27:43 +0000

    Reimplement as calls to the Lookup class.
    git-svn-id: http://svn.code.sf.net/p/dnsjava/code/trunk@909 c76caeb1-94fd-44dd-870f-0c9d92034fc1

--- org/xbill/DNS/dns.java
+++ org/xbill/DNS/dns.java
@@ -10,12 +10,9 @@ import java.io.*;
 import java.net.*;
 
 /**
- * High level API for mapping queries to DNS Records.  Caching is used
- * when possible to reduce the number of DNS requests, and a Resolver
- * is used to perform the queries.  A search path can be set or determined
- * by FindServer, which allows lookups of unqualified names.
- * @see Resolver
- * @see FindServer
+ * A high level API for mapping queries to DNS Records.  This is basically
+ * a wrapper around the Lookup class.
+ * @see Lookup
  *
  * @author Brian Wellington
  */
@@ -28,50 +25,9 @@ private static Name [] searchPath;
 private static boolean searchPathSet;
 private static boolean initialized;
 
-static {
-	initialize();
-}
-
 /* Otherwise the class could be instantiated */
 private
 dns() {}
-
-private static synchronized void
-clearCaches() {
-	Iterator it = caches.entrySet().iterator();
-	while (it.hasNext()) {
-		Cache c = (Cache)it.next();
-		c.clearCache();
-	}
-}
-
-private static synchronized void
-initialize() {
-	if (initialized)
-		return;
-	initialized = true;
-	if (res == null) {
-		try {
-			setResolver(new ExtendedResolver());
-		}
-		catch (UnknownHostException uhe) {
-			System.err.println("Failed to initialize resolver");
-			System.exit(-1);
-		}
-	}
-	if (!searchPathSet)
-		searchPath = FindServer.searchPath();
-	
-	if (caches == null)
-		caches = new HashMap();
-	else
-		clearCaches();
-}
-
-static boolean
-matchType(short type1, short type2) {
-	return (type1 == Type.ANY || type2 == Type.ANY || type1 == type2);
-}
 
 /**
  * Converts an InetAddress into the corresponding domain name
@@ -113,8 +69,7 @@ inaddrString(String s) {
  */
 public static synchronized void
 setResolver(Resolver res) {
-	initialize();
-	dns.res = res;
+	Lookup.setDefaultResolver(res);
 }
 
 /**
@@ -123,30 +78,33 @@ setResolver(Resolver res) {
  */
 public static synchronized Resolver
 getResolver() {
-	return res;
+	return Lookup.getDefaultResolver();
 }
 
 /**
  * Specifies the domains which will be appended to unqualified names before
  * beginning the lookup process.  If this is not set, FindServer will be used.
+ * Unlike the Lookup setSearchPath function, this will silently ignore
+ * invalid names.
  * @see FindServer
  */
 public static synchronized void
 setSearchPath(String [] domains) {
-	if (domains == null || domains.length == 0)
-		searchPath = null;
-	else {
-		List l = new ArrayList();
-		for (int i = 0; i < domains.length; i++) {
-			try {
-				l.add(Name.fromString(domains[i], Name.root));
-			}
-			catch (TextParseException e) {
-			}
-		}
-		searchPath = (Name [])l.toArray(new Name[l.size()]);
+	if (domains == null || domains.length == 0) {
+		Lookup.setDefaultSearchPath((Name []) null);
+		return;
 	}
-	searchPathSet = true;
+
+	List l = new ArrayList();
+	for (int i = 0; i < domains.length; i++) {
+		try {
+			l.add(Name.fromString(domains[i], Name.root));
+		}
+		catch (TextParseException e) {
+		}
+	}
+	searchPath = (Name [])l.toArray(new Name[l.size()]);
+	Lookup.setDefaultSearchPath(searchPath);
 }
 
 /**
@@ -157,12 +115,7 @@ setSearchPath(String [] domains) {
  */
 public static synchronized Cache
 getCache(short dclass) {
-	Cache c = (Cache) caches.get(DClass.toShort(dclass));
-	if (c == null) {
-		c = new Cache(dclass);
-		caches.put(DClass.toShort(dclass), c);
-	}
-	return c;
+	return Lookup.getDefaultCache(dclass);
 }
 
 /**
@@ -173,98 +126,7 @@ getCache(short dclass) {
  */
 public static synchronized Cache
 getCache() {
-	return getCache(DClass.IN);
-}
-
-private static Record []
-lookup(Name name, short type, short dclass, byte cred, int iterations,
-       boolean querysent)
-{
-	Cache cache;
-
-	if (iterations > 6)
-		return null;
-
-	if (Options.check("verbose"))
-		System.err.println("lookup " + name + " " + Type.string(type));
-	cache = getCache(dclass);
-	SetResponse cached = cache.lookupRecords(name, type, cred);
-	if (Options.check("verbose"))
-		System.err.println(cached);
-	if (cached.isSuccessful()) {
-		RRset [] rrsets = cached.answers();
-		List l = new ArrayList();
-		Iterator it;
-		Record [] answers;
-		int i = 0;
-
-		for (i = 0; i < rrsets.length; i++) {
-			it = rrsets[i].rrs();
-			while (it.hasNext()) {
-				l.add(it.next());
-			}
-		}
-
-		return (Record []) l.toArray(new Record[l.size()]);
-	}
-	else if (cached.isNXDOMAIN() || cached.isNXRRSET()) {
-		return null;
-	}
-	else if (cached.isCNAME()) {
-		CNAMERecord cname = cached.getCNAME();
-		return lookup(cname.getTarget(), type, dclass, cred,
-			      ++iterations, false);
-	}
-	else if (cached.isDNAME()) {
-		DNAMERecord dname = cached.getDNAME();
-		Name newname;
-		try {
-			newname = name.fromDNAME(dname);
-		}
-		catch (NameTooLongException e) {
-			return null;
-		}
-		return lookup(newname, type, dclass, cred, ++iterations, false);
-	}
-	else if (querysent) {
-		return null;
-	}
-	else {
-		Record question = Record.newRecord(name, type, dclass);
-		Message query = Message.newQuery(question);
-		Message response;
-
-		try {
-			response = res.send(query);
-		}
-		catch (Exception ex) {
-			return null;
-		}
-
-		short rcode = response.getHeader().getRcode();
-		if (rcode == Rcode.NOERROR || rcode == Rcode.NXDOMAIN)
-			cache.addMessage(response);
-
-		if (rcode != Rcode.NOERROR)
-			return null;
-
-		return lookup(name, type, dclass, cred, iterations, true);
-	}
-}
-
-
-private static Record []
-lookupAppend(Name name, Name suffix, short type, short dclass, byte cred,
-	     int iterations, boolean querysent)
-{
-	try {
-		Name newname = Name.concatenate(name, suffix);
-		return lookup(newname, type, dclass, cred, iterations,
-			      querysent);
-	}
-	catch (NameTooLongException e) {
-		return null;
-	}
+	return Lookup.getDefaultCache(DClass.IN);
 }
 
 /**
@@ -278,40 +140,13 @@ lookupAppend(Name name, Name suffix, short type, short dclass, byte cred,
  */
 public static Record []
 getRecords(String namestr, short type, short dclass, byte cred) {
-	Record [] answers = null;
-	Name name;
-
 	try {
-		name = Name.fromString(namestr, null);
-	} catch (TextParseException e) {
+		Lookup lookup = new Lookup(namestr, type, dclass);
+		lookup.setCredibility(cred);
+		return lookup.run();
+	} catch (Exception e) {
 		return null;
 	}
-
-	if (!Type.isRR(type) && type != Type.ANY)
-		return null;
-
-	if (name.isAbsolute())
-		answers = lookup(name, type, dclass, cred, 0, false);
-	else if (searchPath == null) {
-		answers = lookupAppend(name, Name.root, type, dclass,
-				       cred, 0, false);
-	} else {
-		answers = null;
-
-		if (name.labels() > 1)
-			answers = lookupAppend(name, Name.root, type, dclass,
-					       cred, 0, false);
-
-		for (int i = 0; answers == null && i < searchPath.length; i++)
-			answers = lookupAppend(name, searchPath[i], type,
-					       dclass, cred, 0, false);
-
-		if (answers == null && name.labels() <= 1)
-			answers = lookupAppend(name, Name.root, type, dclass,
-					       cred, 0, false);
-	}
-
-	return answers;
 }
 
 /**
