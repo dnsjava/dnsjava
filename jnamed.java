2b9802e2c0b558bf7896dee4c6b134a81b524289
bwelling <bwelling@c76caeb1-94fd-44dd-870f-0c9d92034fc1>
2004-01-12 00:18:30 +0000

    The configuration file was never closed.
    git-svn-id: http://svn.code.sf.net/p/dnsjava/code/trunk@1144 c76caeb1-94fd-44dd-870f-0c9d92034fc1

--- jnamed.java
+++ jnamed.java
@@ -30,69 +30,76 @@ jnamed(String conffile) throws IOException, ZoneTransferException {
 		return;
 	}
 
-	caches = new HashMap();
-	znames = new HashMap();
-	TSIGs = new HashMap();
+	try {
+		caches = new HashMap();
+		znames = new HashMap();
+		TSIGs = new HashMap();
 
-	BufferedReader br = new BufferedReader(new InputStreamReader(fs));
-	String line = null;
-	while ((line = br.readLine()) != null) {
-		StringTokenizer st = new StringTokenizer(line);
-		if (!st.hasMoreTokens())
-			continue;
-		String keyword = st.nextToken();
-		if (!st.hasMoreTokens()) {
-			System.out.println("Invalid line: " + line);
-			continue;
-		}
-		if (keyword.charAt(0) == '#')
-			continue;
-		if (keyword.equals("primary"))
-			addPrimaryZone(st.nextToken(), st.nextToken());
-		else if (keyword.equals("secondary"))
-			addSecondaryZone(st.nextToken(), st.nextToken());
-		else if (keyword.equals("cache")) {
-			Cache cache = new Cache(st.nextToken());
-			caches.put(new Integer(DClass.IN), cache);
-		}
-		else if (keyword.equals("key"))
-			addTSIG(st.nextToken(), st.nextToken());
-		else if (keyword.equals("port"))
-			ports.add(Integer.valueOf(st.nextToken()));
-		else if (keyword.equals("address")) {
-			String addr = st.nextToken();
-			addresses.add(InetAddress.getByName(addr));
-		} else {
-			System.out.println("ignoring invalid keyword: " +
-					   keyword);
+		InputStreamReader isr = new InputStreamReader(fs);
+		BufferedReader br = new BufferedReader(isr);
+		String line = null;
+		while ((line = br.readLine()) != null) {
+			StringTokenizer st = new StringTokenizer(line);
+			if (!st.hasMoreTokens())
+				continue;
+			String keyword = st.nextToken();
+			if (!st.hasMoreTokens()) {
+				System.out.println("Invalid line: " + line);
+				continue;
+			}
+			if (keyword.charAt(0) == '#')
+				continue;
+			if (keyword.equals("primary"))
+				addPrimaryZone(st.nextToken(), st.nextToken());
+			else if (keyword.equals("secondary"))
+				addSecondaryZone(st.nextToken(),
+						 st.nextToken());
+			else if (keyword.equals("cache")) {
+				Cache cache = new Cache(st.nextToken());
+				caches.put(new Integer(DClass.IN), cache);
+			}
+			else if (keyword.equals("key"))
+				addTSIG(st.nextToken(), st.nextToken());
+			else if (keyword.equals("port"))
+				ports.add(Integer.valueOf(st.nextToken()));
+			else if (keyword.equals("address")) {
+				String addr = st.nextToken();
+				addresses.add(InetAddress.getByName(addr));
+			} else {
+				System.out.println("unknown keyword: " +
+						   keyword);
+			}
+
 		}
 
+		if (ports.size() == 0)
+			ports.add(new Integer(53));
+
+		if (addresses.size() == 0)
+			addresses.add(null);
+
+		Iterator iaddr = addresses.iterator();
+		while (iaddr.hasNext()) {
+			InetAddress addr = (InetAddress) iaddr.next();
+			Iterator iport = ports.iterator();
+			while (iport.hasNext()) {
+				int port = ((Integer)iport.next()).intValue();
+				String addrString;
+				addUDP(addr, port);
+				addTCP(addr, port);
+				if (addr == null)
+					addrString = "0.0.0.0";
+				else
+					addrString = addr.getHostAddress();
+				System.out.println("jnamed: listening on " +
+						   addrString + "#" + port);
+			}
+		}
+		System.out.println("jnamed: running");
 	}
-
-	if (ports.size() == 0)
-		ports.add(new Integer(53));
-
-	if (addresses.size() == 0)
-		addresses.add(null);
-
-	Iterator iaddr = addresses.iterator();
-	while (iaddr.hasNext()) {
-		InetAddress addr = (InetAddress) iaddr.next();
-		Iterator iport = ports.iterator();
-		while (iport.hasNext()) {
-			int port = ((Integer)iport.next()).intValue();
-			String addrString;
-			addUDP(addr, port);
-			addTCP(addr, port);
-			if (addr == null)
-				addrString = "0.0.0.0";
-			else
-				addrString = addr.getHostAddress();
-			System.out.println("jnamed: listening on " +
-					   addrString + "#" + port);
-		}
+	finally {
+		fs.close();
 	}
-	System.out.println("jnamed: running");
 }
 
 public void
@@ -433,7 +440,7 @@ throws IOException
 
 	Message response = new Message(query.getHeader().getID());
 	response.getHeader().setFlag(Flags.QR);
-	if (query.getHeader().getFlag(Flags.RD));
+	if (query.getHeader().getFlag(Flags.RD))
 		response.getHeader().setFlag(Flags.RD);
 	response.addRecord(queryRecord, Section.QUESTION);
 
