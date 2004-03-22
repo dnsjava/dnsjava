5a89f87d1c288708625f024d5ad8293c305c39a6
bwelling <bwelling@c76caeb1-94fd-44dd-870f-0c9d92034fc1>
2004-03-22 18:40:42 +0000

    Fix empty comment.
    git-svn-id: http://svn.code.sf.net/p/dnsjava/code/trunk@1263 c76caeb1-94fd-44dd-870f-0c9d92034fc1

--- org/xbill/DNS/FormattedTime.java
+++ org/xbill/DNS/FormattedTime.java
@@ -51,7 +51,7 @@ format(Date date) {
  * Parses a formatted time string into a Date.
  * @param s The string, in the form YYYYMMDDHHMMSS.
  * @return The Date object.
- * @throws 
+ * @throws TextParseExcetption The string was invalid.
  */
 public static Date
 parse(String s) throws TextParseException {
