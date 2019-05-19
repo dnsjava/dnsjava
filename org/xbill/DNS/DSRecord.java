b5290fd24744ce24c1d25b70fe5eb688eb674c85
Dennis Reichenberg <dennis@swaglos.de>
2019-05-19 13:16:38 +0200

    Revert undesired format changes

--- org/xbill/DNS/DSRecord.java
+++ org/xbill/DNS/DSRecord.java
@@ -13,27 +13,27 @@ package org.xbill.DNS;
 
 public class DSRecord extends DSRecordBase {
 
-static public class Digest
-{
+public static class Digest {
+
 	private Digest() {}
 
 	/** SHA-1 */
-	static public final int SHA1 = 1;
+	public static final int SHA1 = 1;
 
 	/** SHA-256 */
-	static public final int SHA256 = 2;
+	public static final int SHA256 = 2;
 
 	/** GOST R 34.11-94 */
-	static public final int GOST3411 = 3;
+	public static final int GOST3411 = 3;
 
 	/** SHA-384 */
-	static public final int SHA384 = 4;
+	public static final int SHA384 = 4;
 }
 
-public static final int SHA1_DIGEST_ID     = Digest.SHA1;
-public static final int SHA256_DIGEST_ID   = Digest.SHA256;
+public static final int SHA1_DIGEST_ID = Digest.SHA1;
+public static final int SHA256_DIGEST_ID = Digest.SHA256;
 public static final int GOST3411_DIGEST_ID = Digest.GOST3411;
-public static final int SHA384_DIGEST_ID   = Digest.SHA384;
+public static final int SHA384_DIGEST_ID = Digest.SHA384;
 
 private static final long serialVersionUID = -9001819329700081493L;
 
@@ -47,13 +47,13 @@ getObject() {
 /**
  * Creates a DS Record from the given data
  * @param footprint The original KEY record's footprint (keyid).
- * @param alg       The original key algorithm.
- * @param digestid  The digest id code.
- * @param digest    A hash of the original key.
+ * @param alg The original key algorithm.
+ * @param digestid The digest id code.
+ * @param digest A hash of the original key.
  */
 public
-DSRecord(Name name, int dclass, long ttl, int footprint, int alg, int digestid,
-		 byte[] digest)
+DSRecord(Name name, int dclass, long ttl, int footprint, int alg,
+		 int digestid, byte[] digest)
 {
 	super(name, Type.DS, dclass, ttl, footprint, alg, digestid, digest);
 }
