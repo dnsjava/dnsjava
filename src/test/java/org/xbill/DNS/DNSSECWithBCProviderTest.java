1e00fb8a6bbb4f6cd4e820b870ba73b1dfea3b3c
Kenny Ingle <kenny.ingle@gmail.com>
2019-05-30 17:45:07 -0500

    Add Bouncy Castle test (#48)
    * Add Bouncy Castle test
    
    * Remove DNSSECWithLunaProviderTest

---
+++ src/test/java/org/xbill/DNS/DNSSECWithBCProviderTest.java
@@ -0,0 +1,45 @@
+package org.xbill.DNS;
+
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.xbill.DNS.DNSSEC.Algorithm;
+
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.Security;
+import java.security.Signature;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class DNSSECWithBCProviderTest {
+
+	private static final String KEY_ALGORITHM = "RSA";
+	int algorithm = Algorithm.RSASHA1;
+	String bcJCAProvider = "BC";
+	byte[] toSign = "The quick brown fox jumped over the lazy dog.".getBytes();
+
+	@BeforeAll
+	static void setUp() {
+		Security.addProvider(new BouncyCastleProvider());
+	}
+
+	@Test
+	void testSignWithDNSSECAndBCProvider() throws Exception {
+
+		// generate a signature
+		KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance(KEY_ALGORITHM, bcJCAProvider);
+		keyPairGenerator.initialize(512);
+		KeyPair keyPair = keyPairGenerator.generateKeyPair();
+		byte[] signature = DNSSEC.sign(keyPair.getPrivate(), keyPair.getPublic(), algorithm, toSign, bcJCAProvider);
+		assertNotNull(signature);
+
+		// verify the signature
+		Signature verifier = Signature.getInstance(DNSSEC.algString(algorithm), bcJCAProvider);
+		verifier.initVerify(keyPair.getPublic());
+		verifier.update(toSign);
+		boolean verify = verifier.verify(signature);
+		assertTrue(verify);
+	}
+}
