ca745c646d00b81d49a24b4e2464f63e4ed8b4d1
Giampaolo <g.ferraro22@gmail.com>
2019-12-04 09:40:03 +1300

    fixed lint

--- src/main/java/org/xbill/DNS/config/ResolvConfResolverConfigProvider.java
+++ src/main/java/org/xbill/DNS/config/ResolvConfResolverConfigProvider.java
@@ -2,12 +2,12 @@
 package org.xbill.DNS.config;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.InetSocketAddress;
-import java.io.File;
-import java.io.FileInputStream;
 import java.util.StringTokenizer;
 import org.xbill.DNS.SimpleResolver;
 
