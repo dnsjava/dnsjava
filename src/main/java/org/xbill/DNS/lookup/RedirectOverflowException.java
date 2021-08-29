804ecd5125d94f94d9ac829e82e512597ca161e4
Ingo Bauersachs <ingo@jitsi.org>
2021-08-29 15:04:02 +0200

    Make constructor public for unit testing by library users

--- src/main/java/org/xbill/DNS/lookup/RedirectOverflowException.java
+++ src/main/java/org/xbill/DNS/lookup/RedirectOverflowException.java
@@ -1,18 +1,24 @@
 // SPDX-License-Identifier: BSD-2-Clause
 package org.xbill.DNS.lookup;
 
+import lombok.Getter;
+
 /**
  * Thrown if the lookup results in too many CNAME and/or DNAME indirections. This would be the case
  * for example if two CNAME records point to each other.
  */
 public class RedirectOverflowException extends LookupFailedException {
-  /** @deprecated do not use, this class is meant for internal dnsjava usage only. */
+  @Getter private final int maxRedirects;
+
+  /** @deprecated Use {@link RedirectOverflowException#RedirectOverflowException(int)}. */
   @Deprecated
   public RedirectOverflowException(String message) {
     super(message);
+    maxRedirects = 0;
   }
 
-  RedirectOverflowException(int maxRedirects) {
+  public RedirectOverflowException(int maxRedirects) {
     super("Refusing to follow more than " + maxRedirects + " redirects");
+    this.maxRedirects = maxRedirects;
   }
 }
