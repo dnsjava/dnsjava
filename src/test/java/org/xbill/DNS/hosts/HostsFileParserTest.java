036a16f5d9345a22fb8e0327afb4ec6381f0d95d
tian bao <2011xuesong@gmail.com>
2024-11-26 02:06:47 +0800

    Support custom hosts file size (#349)

--- src/test/java/org/xbill/DNS/hosts/HostsFileParserTest.java
+++ src/test/java/org/xbill/DNS/hosts/HostsFileParserTest.java
@@ -172,6 +172,19 @@ class HostsFileParserTest {
   }
 
   @Test
+  void testBigFileCompletelyCachedA() throws IOException {
+    try {
+      System.setProperty("dnsjava.hostsfile.max_size_bytes", 1024 * 1024 * 1024 + "");
+      HostsFileParser hostsFileParser = generateLargeHostsFile("testBigFileCompletelyCachedA");
+      hostsFileParser.getAddressForHost(Name.fromConstantString("localhost-10."), Type.A)
+        .orElseThrow(() -> new IllegalStateException("Host entry not found"));
+      assertEquals(1280, hostsFileParser.cacheSize());
+    } finally {
+      System.clearProperty("dnsjava.hostsfile.max_size_bytes");
+    }
+  }
+
+  @Test
   void testBigFileIsNotCompletelyCachedAAAA() throws IOException {
     HostsFileParser hostsFileParser =
         generateLargeHostsFile("testBigFileIsNotCompletelyCachedAAAA");
