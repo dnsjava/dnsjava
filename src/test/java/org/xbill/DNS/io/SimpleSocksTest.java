12670ce24f14b93c79a7ea9e5c2f5b73028ae34d
Thomas Weiß <thomas.weiss@internetx.com>
2025-01-25 20:52:46 +0100

    implemented simple test for DNS over Socks

--- src/test/java/org/xbill/DNS/io/SimpleSocksTest.java
+++ src/test/java/org/xbill/DNS/io/SimpleSocksTest.java
@@ -1,5 +1,7 @@
 package org.xbill.DNS.io;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.xbill.DNS.*;
 import org.xbill.DNS.Record;
@@ -8,61 +10,53 @@ import java.io.IOException;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class SimpleSocksTest extends AbstractSocksTest {
 
+  @BeforeAll
+  public static void setUp() throws IOException {
+    environment.start();
+  }
+
+  @AfterAll
+  public static void tearDown() throws IOException {
+    environment.stop();
+  }
+
+  private SimpleResolver createResolver(String address, boolean useTCP, String user, String password) throws IOException {
+    SimpleResolver res = address == null ? new SimpleResolver() : new SimpleResolver(address);
+    InetSocketAddress proxyAddress = new InetSocketAddress(InetAddress.getByName("127.0.0.1"), 1080);
+    NioSocks5ProxyConfig config = user == null ? new NioSocks5ProxyConfig(proxyAddress) : new NioSocks5ProxyConfig(proxyAddress, user, password);
+    res.setIoClientFactory(new NioSocks5ProxyFactory(config));
+    res.setTCP(useTCP);
+    return res;
+  }
+
   @Test
   public void testUDP() throws IOException {
-    environment.start();
-
-    SimpleResolver res = new SimpleResolver();
-    InetSocketAddress proxyAddress = new InetSocketAddress(InetAddress.getByName("127.0.0.1"), 1080);
-    NioSocks5ProxyConfig config = new NioSocks5ProxyConfig(proxyAddress);
-    res.setIoClientFactory(new NioSocks5ProxyFactory(config));
-
+    SimpleResolver res = createResolver(null, false, null, null);
     Record rec = Record.newRecord(Name.fromString("simple.test", Name.root), Type.A, DClass.IN);
     Message query = Message.newQuery(rec);
     Message response = res.send(query);
-    System.out.println(response);
-
-    environment.stop();
+    assertNotNull(response);
   }
 
   @Test
   public void testTCP() throws IOException {
-    environment.start();
-
-    SimpleResolver res = new SimpleResolver("10.5.0.2");
-    InetSocketAddress proxyAddress = new InetSocketAddress(InetAddress.getByName("127.0.0.1"), 1080);
-    NioSocks5ProxyConfig config = new NioSocks5ProxyConfig(proxyAddress);
-    res.setIoClientFactory(new NioSocks5ProxyFactory(config));
-    res.setTCP(true);
-
+    SimpleResolver res = createResolver("10.5.0.2", true, null, null);
     Record rec = Record.newRecord(Name.fromString("simple.test", Name.root), Type.A, DClass.IN);
     Message query = Message.newQuery(rec);
     Message response = res.send(query);
-    System.out.println(response);
-
-    environment.stop();
+    assertNotNull(response);
   }
 
   @Test
   public void testAuth() throws IOException {
-    environment.start();
-
-    SimpleResolver res = new SimpleResolver();
-    InetSocketAddress proxyAddress = new InetSocketAddress(InetAddress.getByName("127.0.0.1"), 1080);
-    String socks5User = "me";
-    String socks5Password = "42";
-    NioSocks5ProxyConfig config = new NioSocks5ProxyConfig(proxyAddress, socks5User, socks5Password);
-    res.setIoClientFactory(new NioSocks5ProxyFactory(config));
-
+    SimpleResolver res = createResolver(null, false, "me", "42");
     Record rec = Record.newRecord(Name.fromString("simple.test", Name.root), Type.A, DClass.IN);
     Message query = Message.newQuery(rec);
     Message response = res.send(query);
-    System.out.println(response);
-
-    environment.stop();
+    assertNotNull(response);
   }
-
 }
