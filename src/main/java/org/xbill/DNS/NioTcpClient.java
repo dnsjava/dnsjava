23734e1923e8203e65291a7096664f1cb92c98ff
Ingo Bauersachs <ingo@jitsi.org>
2026-02-13 19:18:38 +0100

    Handle interrupts on NIO selector thread
    Closes #396

--- src/main/java/org/xbill/DNS/NioTcpClient.java
+++ src/main/java/org/xbill/DNS/NioTcpClient.java
@@ -66,11 +66,10 @@ final class NioTcpClient extends NioClient implements TcpIoClient {
   private void closeTcp() {
     registrationQueue.clear();
     EOFException closing = new EOFException("Client is closing");
-    channelMap.forEach(
-        (key, state) -> {
-          state.handleTransactionException(closing);
-          state.handleChannelException(closing);
-        });
+    for (ChannelState state : channelMap.values()) {
+      state.handleTransactionException(closing);
+      state.handleChannelException(closing);
+    }
     channelMap.clear();
   }
 
